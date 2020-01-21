1604974dcfb25be90a9e28721c3d2d65d453045a
harlen lopez <harlen.d.lopez@gmail.com>
2020-01-21 09:45:20 -0800

    server added

---
+++ server.js
@@ -0,0 +1,17 @@
+'use strict'
+
+require('dotenv').config();
+
+const express = require('express');
+const pg = require('pg');
+const PORT = process.env.PORT || 3001;
+const app = express();
+const client = new pg.Client(process.env.DATABASE_URL);
+client.on('error', err => console.error(err));
+const superagent = require('superagent');
+
+app.use(cors());
+
+
+
+app.listen(PORT, () => console.log(`Your server is listening on ${PORT}`));
